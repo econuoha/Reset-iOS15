ba1b8e82433a16b33d448b26a3d6f269def18bb4
Emmanuel Onuoha <emanonuoha@gmail.com>
2022-06-24 00:19:34 -0500

    Initial Commit

---
+++ Reset/ResetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResetApp.swift
+//  Reset
+//
+//  Created by Emmanuel Onouha on 6/24/22.
+//
+
+import SwiftUI
+
+@main
+struct ResetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
